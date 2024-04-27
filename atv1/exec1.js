39e219b66d549fa48a961cc8d431e74b83c04c90
eniobezerra <142815890+eniobezerra@users.noreply.github.com>
2024-04-27 11:38:27 +0000

    atualizacao

---
+++ atv1/exec1.js
@@ -0,0 +1,15 @@
+let prompt = require('prompt-sync')();
+// Leitura do primeiro número e atribuição à variável v1
+let v1;
+v1 = Number(prompt("Digite o primeiro número:"));
+
+// Leitura do segundo número e atribuição à variável v2
+var v2;
+v2 = Number(prompt("Digite o segundo número:"));
+
+// Leitura do terceiro número e atribuição à variável v3
+var v3;
+v3 = Number(prompt("Digite o terceiro número:"));
+
+// Exibição dos números lidos
+console.log("Os números digitados foram: " + v1 + ", " + v2 + " e " + v3);
--- exec1.js
+++ exec1.js
@@ -1,10 +1,12 @@
-console.log("enio ramos");
-console.log("enio ramos");
-console.log("enio ramos");
-console.log("enio ramos");
-console.log("enio ramos");
+
+
 
 let n1 = 0;
 let ne =Number(prompt());
-
+console.log(n1);
+let n1 = 0;
+let ne =Number(prompt());
+console.log(n1);
+let n1 = 0;
+let ne =Number(prompt());
 console.log(n1);
